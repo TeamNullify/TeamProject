57004fa5b4bb445a7106aa5695cc37d8aae749fe
Abdel <abood.abudayyah@hotmail.com>
2023-05-05 15:33:29 -0700

    just comment

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Main {
+    // test
     static UI UIBuilder = new UI();
     public static void main(String[] args){
         UIBuilder.buildUI();
