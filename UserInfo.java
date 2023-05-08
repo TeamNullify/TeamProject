1ad1e8e26e556b3d80a69fb2152c961632a8ae95
mbabazielroy <69889595+mbabazielroy@users.noreply.github.com>
2023-05-07 21:16:56 -0700

    UserInfo

---
+++ UserInfo.java
@@ -0,0 +1,20 @@
+public class UserInfo {
+    private static String userName;
+    private static String userEmail;
+
+    public static String getName() {
+        return userName;
+    }
+
+    public static String getEmail() {
+        return userEmail;
+    }
+
+    public static void setName(String name) {
+        userName = name;
+    }
+
+    public static void setEmail(String email) {
+        userEmail = email;
+    }
+}
