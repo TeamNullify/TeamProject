08ce57b5a1078dd225e041bf94fc2affae374e4f
Nate Mann <129914525+ImaMein@users.noreply.github.com>
2023-06-08 21:54:05 -0700

    Update Info.java

--- Info.java
+++ Info.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+* @author Christopher Yuan, Elroy Mbabazi, Anthony Green, Nathaniel Mann
+*/
+
 public class Info {
 
     private static final String versionNumber = "V0.1";
